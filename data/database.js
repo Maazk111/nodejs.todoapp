49fa064aa4349e34f27200907edc9b5d43036f5d
Muhammad Maaz Khan <maazkhan97111@gmail.com>
2024-09-26 06:13:32 +0500

    fixed engine

--- data/database.js
+++ data/database.js
@@ -7,7 +7,7 @@ export const connectDB = () => {
         mongoose
         .connect(process.env.MONGO_URI , {
             dbName:"backendapi",
-        }).then(() => console.log("DataBase Connected"))
+        }).then((c) => console.log(`DataBase Connected with ${c.connection.host}`))
         .catch( (e) => { console.log(e)});
 
 }
